7fc5bf5e9f1e08913290fcd6ec2fd7b35ffa614f
Muhammadyusufdeveloper <155054579+Muhammadyusufdevloper@users.noreply.github.com>
2024-07-07 12:38:55 +0500

    fil

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+      },
+      width: {
+        container: '100%',
+        maxContainer: '1142px',
+      },
+    },
+  },
+  plugins: [],
+}
